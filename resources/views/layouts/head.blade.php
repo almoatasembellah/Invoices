0962850d7b01bfb4863ec53dad4b29bea3af4ac3
“almoatasembellah” <moatasembellah80@gmail.com>
2023-08-27 20:51:42 +0300

    managing title

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,5 +1,5 @@
 <!-- Title -->
-<title> Invoices - Mutasim </title>
+<title> @yield('title') </title>
 <!-- Favicon -->
 <link rel="icon" href="{{URL::asset('assets/img/brand/favicon.png')}}" type="image/x-icon"/>
 <!-- Icons css -->
